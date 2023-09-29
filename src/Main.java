d71ec1b45c6ea73261699a4a55c0a862deae2039
Alumno <200066@iessanalberto.com>
2023-09-29 10:11:01 +0200

    cambios menores

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,9 @@ public class Main {
                         4. Ejercicio 4: Escribir en fichero de texto
                         5. Ejercicio 5: Escribir en fichero de texto con permanencia
                         6. Ejercicio 6: Copia de seguridad del ejercicio 5
-                        7. Ejercicio 7: BackUp de carpeta
-                        8. Ejercicio 10: PONER NOMBRE
+                        7. Ejercicio 7: BackUp de carpeta completo
+                        8. Ejercicio 7: BackUp de carpeta incremental
+                        9. Ejercicio 10: PONER NOMBRE
                         0. Salir
                         *****************************************************************""");
             opcion = sc.next();
@@ -32,7 +33,8 @@ public class Main {
                 case "5" -> code.ej5NIO.writeFiles();
                 case "6" -> code.ej6NIO.createCopy();
                 case "7" -> code.backUp.createCopy();
-                case "8" -> code.ej10NIO.readFiles();
+                case "8" -> code.backUpIncremental.createCopy();
+                case "9" -> code.ej10NIO.readFiles();
                 default -> System.out.println("");
             }
         }
--- src/code/backUpIncremental.java
+++ src/code/backUpIncremental.java
@@ -1,5 +1,8 @@
 package code;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -7,5 +10,14 @@ public class backUpIncremental {
     public static void createCopy() {
         SimpleDateFormat formateo= new SimpleDateFormat("yyyyMMdd'_'HH-mm-ss");
         Date fecha =new Date(System.currentTimeMillis());
+
+        Path rutaOrigen = Path.of("src/resources/backUp/original/");
+        Path rutaDestino = Path.of("src/resources/backUp/incremental-"+fecha);
+
+        try {
+            Files.copy(rutaOrigen, rutaDestino);
+        } catch (IOException e) {
+            System.err.println("Problemon");
+        }
     }
 }
